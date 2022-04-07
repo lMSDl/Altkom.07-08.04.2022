7cef11cd6c1ec45eed948f700bc37cf657ca6a1c
Paweł Łęczycki <pawel@msd.info.pl>
2022-04-07 10:30:09 +0200

    Aplikacja konsolowa

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { FirstName = "Ewa", LastName = "Ewowska" };
+
+            Console.WriteLine($"Hello {person.FirstName}!");
+        }
+    }
+}
