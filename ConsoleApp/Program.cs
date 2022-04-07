98fdd904cef7584fc17c6a32cde1e774ba6382fc
Paweł Łęczycki <pawel@msd.info.pl>
2022-04-07 12:11:03 +0200

    refactoring

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,10 +8,43 @@ namespace ConsoleApp
 {
     class Program
     {
+        static IConfiguration Configuration { get; set; }
+
         static void Main(string[] args)
         {
+            MakeConfiguration();
+
+
+        }
+
+        private static void ConfigurationDemo()
+        {
+            var section = Configuration.GetSection("Greetings");
+            var subsection = section.GetSection("Targets");
+
+            Console.WriteLine($"{section["Greeting1"]} {subsection["Person"]}!");
+            Console.WriteLine($"{Configuration["Greetings:Greeting2"]} {Configuration["Greetings:Targets:IA"]}!");
+
+            var greetings = new Greetings();
+            Configuration.GetSection("Greetings").Bind(greetings);
+
+            var appConfig = new AppConfig();
+            //package Microsoft.Extensions.Configuration.Binder
+            Configuration.Bind(appConfig);
+
+            //var no = int.Parse(config["NumberOfRepeats"]);
+            var no = Configuration.GetValue<int>("NumberOfRepeats");
+            for (int i = 0; i < no; i++)
+            {
+                Console.WriteLine($"{greetings.Greeting1} {greetings.Targets.Person}!");
+                Console.WriteLine($"{appConfig.Greetings.Greeting2} {appConfig.Greetings.Targets.IA}!");
+            }
+        }
+
+        private static void MakeConfiguration()
+        {
             //package Microsoft.Extensions.Configuration
-            var config = new ConfigurationBuilder()
+            Configuration = new ConfigurationBuilder()
                 //package Microsoft.Extensions.Configuration.FileExtensions
                 //package Microsoft.Extensions.Configuration.Json
                 .AddJsonFile("Configurations/config.json", optional: true)
@@ -22,29 +55,6 @@ namespace ConsoleApp
                 //package NetEscapades.Configuration.Yaml
                 .AddYamlFile("Configurations/config.yaml")
                 .Build();
-
-            var section = config.GetSection("Greetings");
-            var subsection = section.GetSection("Targets");
-            
-            Console.WriteLine($"{section["Greeting1"]} {subsection["Person"]}!");
-            Console.WriteLine($"{config["Greetings:Greeting2"]} {config["Greetings:Targets:IA"]}!");
-
-            var greetings = new Greetings();
-            config.GetSection("Greetings").Bind(greetings);
-
-            var appConfig = new AppConfig();
-            //package Microsoft.Extensions.Configuration.Binder
-            config.Bind(appConfig);
-
-            //var no = int.Parse(config["NumberOfRepeats"]);
-            var no = config.GetValue<int>("NumberOfRepeats");
-            for (int i = 0; i < no; i++)
-            {
-                Console.WriteLine($"{greetings.Greeting1} {greetings.Targets.Person}!");
-                Console.WriteLine($"{appConfig.Greetings.Greeting2} {appConfig.Greetings.Targets.IA}!");
-            }
-
-
         }
     }
 }
